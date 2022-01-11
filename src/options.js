aec3a815fb5f24413d97a1b05e7f5d4789ce089b
Vladimir Bykov <milsinery@icloud.com>
2022-01-11 20:08:31 +0300

    add invalid token hint in options

--- src/options.js
+++ src/options.js
@@ -1,5 +1,6 @@
 const loginInput = document.getElementById('login');
 const loginSet = document.getElementById('loginSet');
+const hint = document.getElementById('hint');
 
 document.getElementById('loginSet').addEventListener('click', () => {
   if (loginInput.value.length === 48) {
@@ -8,5 +9,11 @@ document.getElementById('loginSet').addEventListener('click', () => {
       token: loginInput.value,
     });
     window.close();
+  } else {
+    hint.innerText = "Invalid token";
   }
+});
+
+loginInput.addEventListener('click', () => {
+  hint.innerHTML = `Get API token <a href="https://partners.propellerads.com/#/profile/api"target="_blank">in your account</a>`;
 });

d65d11e7e477053ec61f4813dcaee152f1fd11c8
Vladimir Bykov <milsinery@icloud.com>
2022-05-05 17:35:28 +0300

    add russian language

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,15 @@
-const balanceText = document.getElementsByClassName('info__balance-data');
+// elements for translation
+const spentTitle = document.getElementsByClassName('info__title_spent');
+const statisticsLink = document.getElementsByClassName('info__statistics-link');
+const balanceTitle = document.getElementsByClassName('info__title-balance');
+const addFundsLink = document.getElementsByClassName('info__add-funds-link');
+const updateData = document.getElementsByClassName('info__update-title');
+const specialTitle = document.getElementsByClassName('info__special_title');
+const specialDescription = document.getElementsByClassName('info__special_description');
+
+// elements for data
 const spendingText = document.getElementsByClassName('info__spending-data');
+const balanceText = document.getElementsByClassName('info__balance-data');
 const updateText = document.getElementsByClassName('info__update-data');
 
 const renderOptions = () => {
@@ -9,9 +19,19 @@ const renderOptions = () => {
 };
 
 const renderInfo = (balance, spending, lastUpdateTime) => {
+  // text translation
+  spentTitle[0].innerText = `${chrome.i18n.getMessage('popup_spent')} $`;
+  statisticsLink[0].innerText = chrome.i18n.getMessage('popup_statistics');
+  balanceTitle[0].innerText = `${chrome.i18n.getMessage('popup_balance')} $`;
+  addFundsLink[0].innerText = chrome.i18n.getMessage('popup_add_funds');
+  updateData[0].innerText = chrome.i18n.getMessage('popup_updated_info');
+  specialTitle[0].innerText = chrome.i18n.getMessage('popup_promo_app_title');
+  specialDescription[0].innerText = chrome.i18n.getMessage('popup_promo_app_description');
+
+  // data setting
   balanceText[0].innerText = `${balance}`;
   spendingText[0].innerText = `${spending}`;
-  updateText[0].innerText = `Updated at ${lastUpdateTime}`;
+  updateText[0].innerText = `${lastUpdateTime}`;
 };
 
 chrome.runtime.sendMessage({ action: 'popup_opened' }, (data) => {
